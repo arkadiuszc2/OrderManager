52b6d08f5d874487100558ca2c0148de1f363a70
Arkadiusz <arkadiusz.cieplinski2@gmail.com>
2023-11-08 15:31:01 +0100

    Project intialization and database setup

---
+++ OrderManager/src/test/java/com/mwo/OrderManager/OrderManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mwo.OrderManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
